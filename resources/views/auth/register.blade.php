f474c265266c86e31ba42994074b22257f8858e2
Kilian Tatain Mirail <tatain1@hotmail.com>
2017-09-05 15:34:50 +0200

    desactivation de l'inscription

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,7 +8,7 @@
 				<h3 class="panel-title">Il y a un problème !</h3>
 			</div>
 			<div class="panel-body">
-				<p>Nous sommes désolés mais la page que vous désirez n'existe pas...</p>
+				<p>Nous sommes désolés mais la page que vous désirez n'existe pas... (enfin si, elle existe, mais on l'a désactivé pour l'instant...)</p>
 			</div>
 		</div>
 	</div>
